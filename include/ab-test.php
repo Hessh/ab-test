447313a1579d8448b626eecbe35cb5b094a72ee7
Haakon Hess <haakon.hess@gmail.com>
2020-02-22 11:34:44 +0100

    First commit

---
+++ include/ab-test.php
@@ -0,0 +1,44 @@
+<?php
+
+function abTest() {
+    
+    $randomNumber = rand(0, 1);
+
+    //Hvis cookien er satt og den er lik control: ikke gjør noe
+    if (isset($_COOKIE["variation_id"]) && $_COOKIE["variation_id"] != 1) {
+
+        return;
+
+        //Hvis cookien er satt og den er lik variation: bytt side til variation
+    } else if (isset($_COOKIE["variation_id"]) && $_COOKIE["variation_id"] === 1) {
+
+?>
+<link rel="stylesheet" href="./css/ab-test.css">
+<?php
+
+        return;
+
+        //Hvis cookien ikke er satt og randomNumber er 0: sett cookie til control
+    } else if (!isset($_COOKIE["variation_id"]) && $randomNumber === 0) {
+
+        oneWeekCookie("variation_id", $randomNumber);
+        return;
+
+        //Hvis cookien ikke er satt og randomNumber er 01: sett cookie til variation og bytt side til vatiation
+    } else if (!isset($_COOKIE["variation_id"]) && $randomNumber === 1) {
+
+        oneWeekCookie("variation_id", $randomNumber);
+
+?>
+<link rel="stylesheet" href="./css/ab-test.css">
+<?php
+
+        return;
+    }
+}
+
+// Endre [.domenenavn.tld] til riktig domenenavn, ikke fjern punktum (Linje 41)
+function oneWeekCookie($name, $value)
+{
+    setcookie($name, $value, time() + 30, '/', '.hessh.no', true, true);
+}
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'include/head.php' ?>
+<title>Home</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <h2>This is a test</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia impedit id eveniet esse temporibus! Eos, in,
+        reiciendis impedit id aliquid eaque voluptatibus inventore vitae culpa, sapiente corrupti quasi iusto animi.</p>
+</body>
+
+</html>
